e3f4ae16bc847d48e7639e642eb514e653850ff5
Rocco Caputo <rcaputo@cpan.org>
1998-12-28 14:48:36 -0500

    fixed to match documentation; removed docs

--- lib/POE/Filter/Line.pm
+++ lib/POE/Filter/Line.pm
@@ -1,5 +1,8 @@
 # $Id$
-# Documentation exists after __END__
+
+# Copyright 1998 Rocco Caputo <troc@netrus.net>.  All rights reserved.
+# This program is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
 
 package POE::Filter::Line;
 
@@ -31,47 +34,8 @@ sub get {
 
 sub put {
   my $self = shift;
-  my $raw = join('', @_) . "\x0D\x0A";
+  my $raw = join("\x0D\x0A", @_) . "\x0D\x0A";
 }
 
 ###############################################################################
 1;
-__END__
-
-=head1 NAME
-
-POE::Filter::Line - convert between line- and stream-based IO
-
-=head1 SYNOPSIS
-
-  $line = new POE::Filter::Line();
-
-  $line_with_crlf = $line->put("A line of text.");
-
-  $lines = $line->get("One\x0DTwo\x0AThree\x0D\x0AFour\x0A\x0DFive");
-  print join(':', @$lines), "\n";
-
-=head1 DESCRIPTION
-
-Breaks up a stream into lines, based on any permutation of CR/LF.  Appends
-CR/LF to the ends of lines being sent.
-
-=head1 PUBLIC METHODS
-
-Please see C<POE::Filter> for explanations.
-
-=head1 EXAMPLES
-
-Please see tests/selects.perl for examples of C<POE::Filter::Line>.
-
-=head1 BUGS
-
-None known.
-
-=head1 CONTACT AND COPYRIGHT
-
-Copyright 1998 Rocco Caputo E<lt>troc@netrus.netE<gt>.  All rights reserved.
-This program is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
