2c4c43784739df6abefb9038ba494439a88686a3
Rocco Caputo <rcaputo@cpan.org>
1999-01-14 21:32:35 -0500

    Storable isn't portable across different endianisms; made FreezeThaw the default and added a mandatory warning if Storable is used.
    Reference::get() had subtle problems; rewrote with cleaner code.

--- lib/POE/Filter/Reference.pm
+++ lib/POE/Filter/Reference.pm
@@ -14,17 +14,18 @@ use strict;
 BEGIN {
   local $SIG{'__DIE__'} = 'DEFAULT';
   eval {
-    require Storable;
-    import Storable qw(freeze thaw);
+    require FreezeThaw;
+    import FreezeThaw qw(freeze thaw);
   };
   if ($@ ne '') {
     eval {
-      require FreezeThaw;
-      import FreezeThaw qw(freeze thaw);
+      require Storable;
+      import Storable qw(freeze thaw);
+      warn "Storable isn't endian-safe.  Try FreezeThaw";
     };
   }
   if ($@ ne '') {
-    die "Filter::Reference requires Storable or FreezeThaw";
+    die "Filter::Reference requires FreezeThaw or Storable";
   }
 }
 
@@ -32,7 +33,9 @@ BEGIN {
 
 sub new {
   my $type = shift;
-  my $self = bless { 'framing buffer' => '' }, $type;
+  my $self = bless { 'framing buffer' => '',
+                     'expecting' => 0
+                   }, $type;
   $self;
 }
 
@@ -40,34 +43,24 @@ sub new {
 
 sub get {
   my ($self, $stream) = @_;
-  my $string .= join('', @$stream);
   my @return;
-  
-  my $data;
-  my $i = 1;
-  if(exists($self->{'pre_get'})) {
-    $string =~s/\A(.{$self->{'pre_get'}})//s;
-    my $ick = $self->{pre_got}.$1;
-    push @return,thaw($ick);
-    delete($self->{'pre_get'});
-    delete($self->{'pre_got'});
+
+  $self->{'framing buffer'} .= join('', @$stream);
+
+  # This doesn't allow 0-byte messages.  That's not a problem for
+  # passing frozen references, but it may cause trouble for filters
+  # derived from this code.  Modify according to taste.
+
+  while ($self->{'expecting'} ||
+         ( ($self->{'framing buffer'} =~ s/^(\d+)\0//s) &&
+           ($self->{'expecting'} = $1)
+         )
+  ) {
+    last unless ($self->{'framing buffer'} =~ s/^(.{$self->{'expecting'}})//s);
+    push @return, thaw($1);
+    $self->{'expecting'} = 0;
   }
-  while($string ne "") {
-    die "LOOP EXISTS:" if($i++ == 500);
-    $string =~s/\A(\d+)\0//;
-    my $bytes_to_get = $1;
-    if(length($string) < $bytes_to_get) {
-      $self->{'pre_get'} = $bytes_to_get - length($string);
-      $bytes_to_get = length($string);
-      $string =~s/\A(.{$bytes_to_get})//s;
-      $self->{'pre_got'} = $1;
-      last;
-    } else {
-      $string =~s/\A(.{$bytes_to_get})//s;
-      my $data = $1;
-      push @return,thaw($data);
-    }
-  }
+
   return \@return;
 }
 
