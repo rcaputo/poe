1fd66d65ca0c9dac69ae904bad7540b9f57cf88e
Rocco Caputo <rcaputo@cpan.org>
2001-07-29 18:35:55 -0400

    document get_one_start() and get_one() and why they exist

--- lib/POE/Filter.pm
+++ lib/POE/Filter.pm
@@ -52,21 +52,38 @@ different constructor parameters.
 
 =item get ARRAYREF
 
-get() translates raw data into records as defined by the filter.  It
-accepts a reference to an array of raw data chunks, and it returns a
-reference to an array of complete records.
-
-Drivers' get() methods return ARRAYREFs of raw data chunks suitable
-for passing to filters' put() methods.
+get() translates raw data into records.  What sort of records is
+defined by the specific filter.  The method accepts a reference to an
+array of raw data chunks, and it returns a reference to an array of
+complete records.  The returned ARRAYREF will be empty if there wasn't
+enough information to create a complete record.  Partial records may
+be buffered until subsequent get() calls complete them.
 
   my $records = $filter->get( $driver->get( $filehandle ) );
 
-There needn't be a 1:1 ratio between raw data and logical records.
-Some filters buffer partial records until they are completed in
-subsequent get() calls.
+get() processes and returns as many records as possible.  This is
+faster than one record per call, but it introduces race conditions
+when switching filters.  If you design filters and intend them to be
+switchable, please see get_one_start() and get_one().
 
-get() returns a reference to an empty array if the stream doesn't
-include enough information to complete a record.
+=item get_one_start ARRAYREF
+
+=item get_one
+
+These methods are a second interface to a filter's input translation.
+They split the usual get() into two stages.
+
+get_one_start() accepts an array reference containing unprocessed
+stream chunks.  It adds them to the filter's internal buffer and does
+nothing else.
+
+get_one() takes no parameters and returns an ARRAYREF of zero or more
+complete records from the filter's buffer.  Unlike the plain get()
+method, get_one() is not greedy.  It returns as few records as
+possible, preferrably just zero or one.
+
+get_one_start() and get_one() reduce or eliminate race conditions when
+switching filters in a wheel.
 
 =item put ARRAYREF
 
@@ -80,9 +97,13 @@ The list reference it returns may be passed directly to a driver.
 
 =item get_pending
 
-get_pending() returns a filter's partial input buffer, clearing it in
-the process.  The ReadWrite wheel uses this for hot-swapping filters;
-it gives partial input buffers to the next filter.
+get_pending() returns a filter's partial input buffer.  Unlike
+previous versions, the filter's input buffer is B<not> cleared.  The
+ReadWrite wheel uses this for hot-swapping filters; it gives partial
+input buffers to the next filter.
+
+get_pending() returns undef if nothing is pending.  This is different
+from get() and get_one().
 
 Filters don't have output buffers.  They accept complete records and
 immediately pass the serialized information to a driver's queue.
