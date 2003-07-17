d2b2eb3f831ccb02dbf873f49ba0219972893a47
Rocco Caputo <rcaputo@cpan.org>
2003-07-17 19:25:51 -0400

    Apply Leif Gustafson's patch to pass conduit types on to POE::Pipe::TwoWay.

--- lib/POE/Wheel/Run.pm
+++ lib/POE/Wheel/Run.pm
@@ -123,10 +123,34 @@ sub new {
   my $user_id  = delete $params{User};
   my $group_id = delete $params{Group};
 
+  # The following $stdio_pipe_type is new.  $conduit is kept around
+  # for now to preserve the logic of the rest of the module.  This
+  # change allows a Session using POE::Wheel::Run to define the type
+  # of pipe to be created for stdin and stdout.  Read the POD on
+  # Conduit.  However, the documentation lies, because if Conduit is
+  # undefined, $stdio_pipe_type is set to undefined (so the default
+  # pipe type provided by POE::Pipe::TwoWay will be used). Otherwise,
+  # $stdio_pipe_type determines what type of pipe Pipe:TwoWay creates
+  # unless it's 'pty'.
+
   my $conduit = delete $params{Conduit};
-  $conduit = 'pipe' unless defined $conduit;
-  croak "$type needs a known Conduit type (pty or pipe, not $conduit)"
-    if $conduit ne 'pipe' and $conduit ne 'pty';
+  my $stdio_pipe_type;
+  if (defined $conduit) {
+    croak "$type\'s Conduit type ($conduit) is unknown"
+      if (
+        $conduit ne 'pipe' and
+        $conduit ne 'pty'  and
+        $conduit ne 'socketpair' and
+        $conduit ne 'inet'
+      );
+    unless ($conduit eq "pty") {
+      $stdio_pipe_type = $conduit;
+      $conduit = "pipe";
+    }
+  }
+  else {
+    $conduit = "pipe";
+  }
 
   my $stdin_event  = delete $params{StdinEvent};
   my $stdout_event = delete $params{StdoutEvent};
@@ -218,11 +242,9 @@ sub new {
     # We make more pipes than strictly necessary in case someone wants
     # to turn some on later.  Uses a TwoWay pipe for STDIN/STDOUT and
     # a OneWay pipe for STDERR.  This may save 2 filehandles if
-    # socketpair() is available.
-    my $pipe_type;
-    $pipe_type = "pipe" if $^O eq "cygwin";
+    # socketpair() is available and no other $stdio_pipe_type is selected.
     ($stdin_read, $stdout_write, $stdout_read, $stdin_write) =
-      POE::Pipe::TwoWay->new($pipe_type);
+      POE::Pipe::TwoWay->new($stdio_pipe_type);
     croak "could not make stdin pipe: $!"
       unless defined $stdin_read and defined $stdin_write;
     croak "could not make stdout pipe: $!"
@@ -1062,7 +1084,15 @@ new() accepts lots of stuff.  Each parameter is name/value pair.
 =item Conduit
 
 C<Conduit> describes how Wheel::Run should talk with the child
-process.  It may either be 'pipe' (the default), or 'pty'.
+process.  By default it will try various forms of inter-process
+communication to build a pipe between the parent and child processes.
+If a particular method is preferred, it can be set to "pipe",
+"socketpair", or "inet".  It may also be set to "pty" if the child
+process should have its own pseudo tty.
+
+The reasons to define this parameter would be if you want to use
+"pty", if the default pipe type doesn't work properly on your
+system, or the default pipe type's performance is poor.
 
 Pty conduits require the IO::Pty module.
 
