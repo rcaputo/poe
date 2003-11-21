0b57b4ba5225ebb00baf44888e36a787b9fe0725
Rocco Caputo <rcaputo@cpan.org>
2003-11-21 05:01:27 -0500

    Exploratory commit of the new $VERSION code.

--- lib/POE/Kernel.pm
+++ lib/POE/Kernel.pm
@@ -5,7 +5,7 @@ package POE::Kernel;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = (qw($Revision$ ))[1];
+$VERSION = do {my@r=(q$Revision$=~/\d+/g);sprintf"%d."."%04d"x$#r,@r};
 
 use POE::Queue::Array;
 use POSIX qw(errno_h fcntl_h sys_wait_h);
