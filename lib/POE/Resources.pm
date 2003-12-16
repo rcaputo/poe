5213cacc034e79e0cd6d57e42cf36ff460cef0ce
Rocco Caputo <rcaputo@cpan.org>
2003-12-16 04:55:42 -0500

    Support auto-loading POE::XS::Resource::Foo if it's available.  Fall back to POE::Resource::Foo if the XS version isn't available or cannot load.

--- lib/POE/Resources.pm
+++ lib/POE/Resources.pm
@@ -5,19 +5,26 @@ use strict;
 use vars qw($VERSION);
 $VERSION = do {my@r=(q$Revision$=~/\d+/g);sprintf"%d."."%04d"x$#r,@r};
 
+my @resources = qw(
+  POE::XS::Resource::Extrefs
+  POE::XS::Resource::SIDs
+  POE::XS::Resource::Signals
+  POE::XS::Resource::Aliases
+  POE::XS::Resource::FileHandles
+  POE::XS::Resource::Events
+  POE::XS::Resource::Sessions
+);
+
 sub initialize {
-    my $package = (caller())[0];
-    eval qq|
-    package $package;
-    use POE::Resource::Extrefs;     # Extra reference counts.
-    use POE::Resource::SIDs;        # Session IDs.
-    use POE::Resource::Signals;     # Signals.
-    use POE::Resource::Aliases;     # Aliases.
-    use POE::Resource::FileHandles; # File handles.
-    use POE::Resource::Events;      # Events.
-    use POE::Resource::Sessions;    # Sessions.
-    |;
-    die if $@;
+  my $package = (caller())[0];
+
+  foreach my $resource (@resources) {
+    eval "package $package; use $resource";
+    if ($@) {
+      redo if $@ =~ /^Can't locate/ and $resource =~ s/::XS::/::/;
+      die;
+    }
+  }
 }
 
 1;
