fc22b4017f6f084933e7776eb7009240abbba910
Matt Cashner <sungo@sungo.us>
2004-01-25 06:14:34 -0500

    add a better error condition in the case of a POST request in a 0.9 transaction.

--- lib/POE/Filter/HTTPD.pm
+++ lib/POE/Filter/HTTPD.pm
@@ -175,8 +175,16 @@ sub get {
 #    print length($buf)."-".$r->content_length()."\n";
 
     my $cl = $r->content_length();
-    return [ $self->build_error(RC_LENGTH_REQUIRED, "No content length found.") ] 
-        unless defined $cl;
+    warn $self->{'httpd_client_proto'};
+    unless(defined $cl) {
+        if($self->{'httpd_client_proto'} == 9) {
+            return [ $self->build_error(RC_BAD_REQUEST,  "POST request detected in an HTTP 0.9 transaction. POST is not a valid HTTP 0.9 transaction type. Please verify your HTTP version and transaction content.") ];
+
+        } else { 
+            return [ $self->build_error(RC_LENGTH_REQUIRED, 
+                        "No content length found.") ];
+        }
+    }
         
     return [ $self->build_error(RC_BAD_REQUEST, "Content length contains non-digits.") ] 
         unless $cl =~ /^\d+$/;
