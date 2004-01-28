7719a26efba112902a0e8e683a79951218f3c426
Rocco Caputo <rcaputo@cpan.org>
2004-01-27 19:18:16 -0500

    Bump POE.pm up to 0.28.  META.yml doesn't change, but cvs still includes it here.  Silly cvs.

--- lib/POE.pm
+++ lib/POE.pm
@@ -7,7 +7,7 @@ use strict;
 use Carp;
 
 use vars qw($VERSION $REVISION);
-$VERSION = '0.2701';
+$VERSION = '0.28';
 $REVISION = do {my@r=(q$Revision$=~/\d+/g);sprintf"%d."."%04d"x$#r,@r};
 
 sub import {
