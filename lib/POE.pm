b6c9ae041224a0152cc4428999df1a7d21ec8a34
Rocco Caputo <rcaputo@cpan.org>
2004-02-02 17:32:12 -0500

    Bump the version to 0.2802.  This will be a CPAN-released update for the ill-fated 0.28.  I figure, why burn a hundredth version for a fixup, especially since we're not entirely sure it's gold?

--- lib/POE.pm
+++ lib/POE.pm
@@ -7,7 +7,7 @@ use strict;
 use Carp;
 
 use vars qw($VERSION $REVISION);
-$VERSION = '0.2801';
+$VERSION = '0.2802';
 $REVISION = do {my@r=(q$Revision$=~/\d+/g);sprintf"%d."."%04d"x$#r,@r};
 
 sub import {
