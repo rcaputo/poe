1b531dba39350c55df948486f28b03d56fa7befe
Rocco Caputo <rcaputo@cpan.org>
2004-04-27 01:21:19 -0400

    Fix the version number so CPAN won't index it.

--- lib/POE.pm
+++ lib/POE.pm
@@ -7,7 +7,7 @@ use strict;
 use Carp;
 
 use vars qw($VERSION $REVISION);
-$VERSION = '0.2804';
+$VERSION = '0.28_04';
 $REVISION = do {my@r=(q$Revision$=~/\d+/g);sprintf"%d."."%04d"x$#r,@r};
 
 sub import {
