ab6f4edbef740efe7ea2c365ad9dac4e5f6694a6
Rocco Caputo <rcaputo@cpan.org>
2004-11-26 22:25:25 -0500

    Increase minor version for another 0.30 fix release.  Hopefully the installer will succeed on more CPAN testers' machines.

--- lib/POE.pm
+++ lib/POE.pm
@@ -7,7 +7,7 @@ use strict;
 use Carp;
 
 use vars qw($VERSION $REVISION);
-$VERSION = '0.3002';
+$VERSION = '0.3003';
 $REVISION = do {my@r=(q$Revision$=~/\d+/g);sprintf"%d."."%04d"x$#r,@r};
 
 sub import {
