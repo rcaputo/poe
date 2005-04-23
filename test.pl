12e00dc0cfd89370745a6c722635bd3eff9cf41b
Rocco Caputo <rcaputo@cpan.org>
2005-04-22 20:27:23 -0400

    Add some demo comments about how to focus on just one event loop's tests.

--- test.pl
+++ test.pl
@@ -18,14 +18,17 @@ $ENV{PERL_DL_NONLAZY} = 1;
 my @test_files = gather_test_files();
 die "*** Can't find test files" unless @test_files;
 
-# Stop the tests at the one that crashes only when "make test" is
-# running.
+# Various test filtering thingies.
 
+# Stop the tests at one that tends to dump core.
 #my $x = @test_files;
 #while (@test_files) {
 #  last if $test_files[-1] eq "tests/30_loops/50_tk/ses_session.t";
 #  pop @test_files;
 #}
+
+# Run the tests for a particular event loop.
+# @test_files = grep /50_tk/, @test_files;
 
 runtests(@test_files);
 exit;
