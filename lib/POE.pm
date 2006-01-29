60690473ffeb21d46404eb033deb6f631ca988e9
Rocco Caputo <rcaputo@cpan.org>
2006-01-29 15:45:38 -0500

    Bump up the version for a bugfix.

--- lib/POE.pm
+++ lib/POE.pm
@@ -7,7 +7,7 @@ use strict;
 use Carp qw( croak );
 
 use vars qw($VERSION $REVISION);
-$VERSION = '0.33';
+$VERSION = '0.3301';
 $REVISION = do {my@r=(q$Revision$=~/\d+/g);sprintf"%d."."%04d"x$#r,@r};
 
 sub import {
