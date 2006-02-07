b449061612f25b8980ed13abc9d30ccc98ce7604
Rocco Caputo <rcaputo@cpan.org>
2006-02-07 09:35:22 -0500

    Remove ExtUtils::AutoInstall from the installer to support ActiveState's PPD builder.  ActiveState's builder is still trivially broken months (years?) after I reported that ExtUtils::AutoInstall needs a configured CPAN.pm.  So I give up.
    The questionnaire is replaced by a simple list of missing or outdated
    dependancies.  ExtUtils::MakeMaker will still enforce our few hard
    requirements.

--- mylib/Makefile-5005.pm
+++ mylib/Makefile-5005.pm
@@ -21,92 +21,66 @@ use PoeBuildInfo qw(
 open(TOUCH, ">>CHANGES") and close TOUCH;
 open(TOUCH, ">>META.yml") and close TOUCH;
 
-### Ensure ExtUtils::AutoInstall is installed.
+### Some advisory dependancy testing.
 
-eval "require ExtUtils::AutoInstall";
-if ($@) {
-  warn(
-    "\n",
-    "====================================================================\n",
-    "\n",
-    "POE's installer magic requires ExtUtils::AutoInstall.  POE comes\n",
-    "with its own version, but it is usually out of date and won't be\n",
-    "installed.  You should install the most recent version at your\n",
-    "earliest convenience.\n",
-    "\n",
-    "====================================================================\n",
-    "\n",
-  );
-  eval "require './mylib/ExtUtils/AutoInstall.pm'";
-  die if $@;
+sub check_for_modules {
+  my ($dep_type, @modules) = @_;
+
+  my @failures;
+  while (@modules) {
+    my $module  = shift @modules;
+    my $target  = shift @modules;
+
+    my $version = eval "use $module; return \$$module\::VERSION";
+
+    if ($@) {
+      push(
+        @failures,
+        "***   $module $target could not be loaded.\n"
+      );
+    }
+    elsif ($version < $target) {
+      push(
+        @failures,
+        "***   $module $target is $dep_type, " .
+        "but version $version is installed.\n"
+      );
+    }
+  }
+
+  if (@failures) {
+    warn(
+      "*** Some $dep_type features may not be available:\n",
+      @failures,
+    );
+  }
 }
 
-### Some things we can do on Windows.
-
-my @windows_options;
-if ($^O eq "MSWin32") {
-  @windows_options = (
-    "Optional modules for Windows POE::Wheel::Run support." => [
-        -default => 0,
-        'Win32API::File'  => '0.05',
-        'Win32::Console'  => '0.031',
-    ],
-  );
-}
-
-### Prompt for additional modules.
-
-ExtUtils::AutoInstall->import(
-  -version => '0.50',
-  -core => [ CORE_REQUIREMENTS ],
-  "Recommended modules to increase timer/alarm/delay accuracy." => [
-      -default      => 0,
-      RECOMMENDED_TIME_HIRES,
-  ],
-  "Optional modules to speed up large-scale clients/servers." => [
-      -default   => 0,
-      'IO::Poll' => 0.01,
-  ],
-  "Optional modules for IPv6 support." => [
-      -default  => 0,
-      'Socket6' => 0.14,
-  ],
-  "Optional modules for controlling full-screen programs (e.g. vi)." => [
-      -default  => 0,
-      'IO::Pty' => '1.02',
-  ],
-  "Optional modules for marshaling/serializing data." => [
-      -default         => 0,
-      'Storable'       => '2.12',
-      'Compress::Zlib' => '1.33',
-  ],
-  "Optional modules for web applications (client & server)." => [
-      -default => 0,
-      'LWP'            => '5.79',
-      'URI'            => '1.30',
-  ],
-  "Optional modules for Curses text interfaces." => [
-      -default => 0,
-      'Curses' => '1.08',
-  ],
-  "Optional modules for console (command line) interfaces." => [
-      -default        => 0,
-      'Term::ReadKey' => '2.21',
-      'Term::Cap'     => '1.09',
-  ],
-  "Optional modules for Gtk+ graphical interfaces." => [
-      -default => 0,
-      'Gtk'    => '0.7009',
-  ],
-  "Optional modules for Tk graphical interfaces." => [
-      -default => 0,
-      'Tk'     => '800.027',
-  ],
-  "Optional modules for Event.pm support." => [
-      -default => 0,
-      'Event'  => '1.00',
-  ],
-  @windows_options,
+check_for_modules("required", CORE_REQUIREMENTS);
+check_for_modules(
+  "optional",
+  "Compress::Zlib"  => 1.33,
+  "Curses"          => 1.08,
+  "Event"           => 1.00,
+  "Gtk"             => 0.7009,
+  "IO::Poll"        => 0.01,
+  "IO::Pty"         => 1.02,
+  "LWP"             => 5.79,
+  "Socket6"         => 0.14,
+  "Storable"        => 2.12,
+  "Term::Cap"       => 1.09,
+  "Term::ReadKey"   => 2.21,
+  RECOMMENDED_TIME_HIRES,
+  "Tk"              => 800.027,
+  "URI"             => 1.30,
+  (
+    ($^O eq "MSWin32")
+    ? (
+      "Win32::Console" => 0.031,
+      "Win32API::File" => 0.05,
+    )
+    : ()
+  ),
 );
 
 ### Generate dynamic test files.
@@ -129,8 +103,7 @@ system($^X, "mylib/gen-tests.perl") and die "couldn't generate tests: $!";
 # poe_report.xml: Makefile
 # \cI$^X mylib/testreport.pl
 sub MY::postamble {
-  return ExtUtils::AutoInstall::postamble() .
-    <<EOF;
+  return <<EOF;
 
 ppmdist: pm_to_blib
 \cI\$(TAR) --exclude '*/man[13]*' -cvf \\
--- mylib/PoeBuildInfo.pm
+++ mylib/PoeBuildInfo.pm
@@ -22,13 +22,13 @@ use vars qw(@EXPORT_OK);
 sub CORE_REQUIREMENTS () {
   (
     "Carp"               => 0,
+    "Errno"              => 1.09,
     "Exporter"           => 0,
+    "File::Spec"         => 0.87,
+    "Filter::Util::Call" => 1.06,
     "IO"                 => 1.20,
     "POSIX"              => 1.02,
     "Socket"             => 1.7,
-    "Filter::Util::Call" => 1.06,
-    "File::Spec"         => 0.87,
-    "Errno"              => 1.09,
     "Test::Harness"      => 2.26,
   )
 }
@@ -50,21 +50,21 @@ sub CLEAN_FILES () {
     coverage.report
     poe_report.xml
     run_network_tests
-    tests/20_resources/10_perl/*
-    tests/20_resources/10_perl
-    tests/20_resources/20_xs/*
-    tests/20_resources/20_xs
-    tests/30_loops/10_select/*
-    tests/30_loops/10_select
-    tests/30_loops/20_poll/*
-    tests/30_loops/20_poll
-    tests/30_loops/30_event/*
-    tests/30_loops/30_event
-    tests/30_loops/40_gtk/*
-    tests/30_loops/40_gtk
-    tests/30_loops/50_tk/*
-    tests/30_loops/50_tk
     test-output.err
+    tests/20_resources/10_perl
+    tests/20_resources/10_perl/*
+    tests/20_resources/20_xs
+    tests/20_resources/20_xs/*
+    tests/30_loops/10_select
+    tests/30_loops/10_select/*
+    tests/30_loops/20_poll
+    tests/30_loops/20_poll/*
+    tests/30_loops/30_event
+    tests/30_loops/30_event/*
+    tests/30_loops/40_gtk
+    tests/30_loops/40_gtk/*
+    tests/30_loops/50_tk
+    tests/30_loops/50_tk/*
   );
   "@clean_files";
 }
