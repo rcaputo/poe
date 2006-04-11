521e6e3558944bdc95625289c265148876b50f43
Rocco Caputo <rcaputo@cpan.org>
2006-04-10 23:36:15 -0400

    Next development version.

--- lib/POE.pm
+++ lib/POE.pm
@@ -7,7 +7,7 @@ use strict;
 use Carp qw( croak );
 
 use vars qw($VERSION $REVISION);
-$VERSION = '0.3401';
+$VERSION = '0.34_02';
 $REVISION = do {my($r)=(q$Revision$=~/(\d+)/);sprintf"1.%04d",$r};
 
 sub import {
