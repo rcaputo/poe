22b66329bb38850de026aa6dfa6e257b4dc3d051
Adam Kennedy <adamk@cpan.org>
2006-04-11 10:06:53 -0400

    Whenever you work with relative time, you have to measure "now" absolutely as soon as possible. Moved the time() call from when delay_set sets the actual alarm to the very top, which is much closer to where the caller meant "now" to mean.

--- lib/POE/Kernel.pm
+++ lib/POE/Kernel.pm
@@ -1938,6 +1938,12 @@ sub alarm_adjust {
 # Time::HiRes'.
 
 sub delay_set {
+  # Always always always grab time() ASAP, so that the eventual
+  # time we set the alarm for is as close as possible to the time
+  # at which they ASKED for the delay, not when we actually set it.
+  my $t = time();
+
+  # And now continue as normal
   my ($self, $event_name, $seconds, @etc) = @_;
 
   if (ASSERT_USAGE) {
@@ -1966,7 +1972,7 @@ sub delay_set {
 
   return $self->_data_ev_enqueue
     ( $kr_active_session, $kr_active_session, $event_name, ET_ALARM, [ @etc ],
-      (caller)[1,2], $kr_active_event, time() + $seconds,
+      (caller)[1,2], $kr_active_event, $t + $seconds,
     );
 }
 
