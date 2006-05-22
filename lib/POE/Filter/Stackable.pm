cadff1ff9a63b11f86657efc104d61543166034e
Matt Sickler <imMute@msk4.com>
2006-05-22 17:57:36 -0400

    Fixed Stackable.pm to make Filter optional again.

--- lib/POE/Filter/Stackable.pm
+++ lib/POE/Filter/Stackable.pm
@@ -26,10 +26,9 @@ sub new {
   croak "$type must be given an even number of parameters" if @_ & 1;
   my %params = @_;
 
+  $params{Filters} = [ ] unless defined $params{Filters};
   # Sanity check the filters
-  if ( exists $params{Filters} and defined $params{Filters}
-       and ref( $params{Filters} ) and ref( $params{Filters} ) eq 'ARRAY'
-       and scalar @{ $params{Filters} } > 0 ) {
+  if ( ref $params{Filters} eq 'ARRAY') {
 
     # Check the elements
     foreach my $elem ( @{ $params{Filters} } ) {
