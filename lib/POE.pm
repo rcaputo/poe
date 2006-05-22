c5a561e1ca4d401a84f7cf35ac7372b74c214307
Rocco Caputo <rcaputo@cpan.org>
2006-05-21 22:16:40 -0400

    Bump the version for release.

--- lib/POE.pm
+++ lib/POE.pm
@@ -7,7 +7,7 @@ use strict;
 use Carp qw( croak );
 
 use vars qw($VERSION $REVISION);
-$VERSION = '0.34_02';
+$VERSION = '0.35';
 $REVISION = do {my($r)=(q$Revision$=~/(\d+)/);sprintf"1.%04d",$r};
 
 sub import {
