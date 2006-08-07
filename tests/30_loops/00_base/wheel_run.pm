13b777540a4fbb70cff84aad499adaa03100b95f
Rocco Caputo <rcaputo@cpan.org>
2006-08-06 20:55:42 -0400

    Skip the Wheel::Run tests if $^O eq "MSWin32" and Win32::Console is not installed.  Some relatively new Win32 distributions advertise $^O identically to ActivePerl but don't include the same set of modules as that distribution.  Resolves rt.cpan.org ticket 20574 from Adam Kennedy.

--- tests/30_loops/00_base/wheel_run.pm
+++ tests/30_loops/00_base/wheel_run.pm
@@ -17,8 +17,14 @@ BEGIN {
     $error = "$^O does not support fork";
   }
 
-  if ($^O eq "MSWin32" and exists $INC{"Event.pm"}) {
-    $error = "$^O\'s fork() emulation breaks Event";
+  if ($^O eq "MSWin32") {
+    eval 'use Win32::Console';
+    if ($@) {
+      $error = "$^O needs Win32::Console for this test";
+    }
+    elsif (exists $INC{"Event.pm"}) {
+      $error = "$^O\'s fork() emulation breaks Event";
+    }
   }
 
   if ($error) {
