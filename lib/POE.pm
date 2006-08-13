d0131a59c85877b6c376be4c329f4d038b6e9f3b
Rocco Caputo <rcaputo@cpan.org>
2006-08-13 02:11:37 -0400

    Some CPAN testers failures have been addressed.  Time for 0.3601.

--- lib/POE.pm
+++ lib/POE.pm
@@ -7,7 +7,7 @@ use strict;
 use Carp qw( croak );
 
 use vars qw($VERSION $REVISION);
-$VERSION = '0.36';
+$VERSION = '0.3601';
 $REVISION = do {my($r)=(q$Revision$=~/(\d+)/);sprintf"1.%04d",$r};
 
 sub import {
