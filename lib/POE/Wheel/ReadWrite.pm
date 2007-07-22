aa8757f5abf20c52b02ceb5a5e4fbb87d72616d9
Rocco Caputo <rcaputo@cpan.org>
2007-07-22 08:37:07 -0400

    Apply Gareth's patch for an optional synchronous put() and a flush() method with which to flush the wheel's buffer on command.  Resolves rt.cpan.org ticket 20328.

--- lib/POE/Wheel/ReadWrite.pm
+++ lib/POE/Wheel/ReadWrite.pm
@@ -28,6 +28,7 @@ sub DRIVER_BUFFERED_OUT_OCTETS () { 13 }
 sub STATE_WRITE                () { 14 }
 sub STATE_READ                 () { 15 }
 sub UNIQUE_ID                  () { 16 }
+sub AUTOFLUSH                  () { 17 }
 
 sub CRIMSON_SCOPE_HACK ($) { 0 }
 
@@ -132,6 +133,7 @@ sub new {
     undef,                            # STATE_READ
     # Unique ID.
     &POE::Wheel::allocate_wheel_id(), # UNIQUE_ID
+    delete $params{AutoFlush},         # AUTOFLUSH
   ], $type;
 
   if (scalar keys %params) {
@@ -423,6 +425,15 @@ sub put {
     $self->[DRIVER_BUFFERED_OUT_OCTETS] =
     $self->[DRIVER_BOTH]->put($self->[FILTER_OUTPUT]->put(\@chunks));
 
+  if (
+    $self->[AUTOFLUSH] &&
+    $new_buffered_out_octets and !$old_buffered_out_octets
+  ) {
+    $old_buffered_out_octets = $new_buffered_out_octets;
+    $self->flush();
+    $new_buffered_out_octets = $self->[DRIVER_BUFFERED_OUT_OCTETS];
+  }
+
   # Resume write-ok if the output buffer gets data.  This avoids
   # redundant calls to select_resume_write(), which is probably a good
   # thing.
@@ -633,6 +644,14 @@ sub shutdown_output {
   $poe_kernel->select_write($self->[HANDLE_OUTPUT], undef);
 }
 
+# Flush the output handle
+sub flush {
+  my $self = shift;
+  return unless defined $self->[HANDLE_OUTPUT];
+  $poe_kernel->call($poe_kernel->get_active_session(),
+        $self->[STATE_WRITE], $self->[HANDLE_OUTPUT]);
+}
+
 ###############################################################################
 1;
 
@@ -676,6 +695,9 @@ POE::Wheel::ReadWrite - buffered non-blocking I/O
     HighEvent => $high_mark_event,  # Event to emit when high-water reached
     LowMark   => $low_mark_octets,  # Outgoing low-water mark
     LowEvent  => $low_mark_event,   # Event to emit when low-water reached
+
+    # Experimental: If true, flush output synchronously during put()
+    AutoFlush => $boolean,
   );
 
   $wheel->put( $something );
@@ -706,6 +728,9 @@ POE::Wheel::ReadWrite - buffered non-blocking I/O
   # To shutdown a wheel's socket(s).
   $wheel->shutdown_input();
   $wheel->shutdown_output();
+
+  # Experimental: Flush a wheel's output on command.
+  $wheel->flush();
 
 =head1 DESCRIPTION
 
@@ -832,6 +857,15 @@ wheel's input and output sockets.
 =item get_driver_out_messages
 
 Return driver statistics.
+
+=item flush
+
+Though the watermarks affect how often a wheel is flushed, in some
+cases you might want to manually flush a smaller output, such as
+before shutdown_output.
+
+This method is experimental.  Its behavior may change or it may
+disappear outright.  Please let us know whether it's useful.
 
 =back
 
