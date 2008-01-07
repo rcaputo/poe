ebcf69f20395caf3c80473e064cb300f6cfdceeb
Rocco Caputo <rcaputo@cpan.org>
2008-01-07 06:09:02 -0500

    Redocument.

--- lib/POE/Driver.pm
+++ lib/POE/Driver.pm
@@ -23,67 +23,113 @@ __END__
 
 =head1 NAME
 
-POE::Driver - an abstract file driver
+POE::Driver - An API for abstract buffered file I/O.
 
 =head1 SYNOPSIS
 
-  $driver = POE::Driver::Something->new();
-  $arrayref_of_data_chunks = $driver->get($filehandle);
-  $queue_octets = $driver->put($arrayref_of_data_chunks);
-  $queue_octets = $driver->flush($filehandle);
-  $queue_messages = $driver->get_out_messages_buffered();
+This is a contrived example of how POE::Filter and POE::Driver objects
+may be used in a stand-alone application.
+
+  my $driver = POE::Driver::SysRW->new();
+  my $filter = POE::Filter::Line->new();
+
+  my $list_of_octet_chunks = $filter->put("A line of text.");
+
+  $driver->put( $list_of_octet_chunks );
+
+  my $octets_remaining_in_buffer = $driver->flush($filehandle);
+  die "couldn't flush everything" if $octets_remaining_in_buffer;
+
+  while (1) {
+    my $octets_list = $driver->get($filehandle);
+    die $! unless defined $octets_list;
+
+    $filter->get_one_start($octets_list);
+    while (my $line = $filter->get_one()) {
+      print "Input: $line\n";
+    }
+  }
+
+Most programs will use POE::Filter and POE::Driver objects as
+parameters to POE::Wheel constructors.  See the synopses for
+particular classes for details.
 
 =head1 DESCRIPTION
 
-Drivers implement generic interfaces to low-level file I/O.  Wheels
-use them to read and write files, sockets, and other things without
-needing to know the details for doing so.
+POE::Driver is a common API for I/O drivers that can read from and
+write to various files, sockets, pipes, and other devices.
 
-=head1 PUBLIC DRIVER METHODS
+POE "drivers" implement the specifics of reading and writing to
+devices.  Drivers plug into POE::Wheel objects so that wheels may
+support a large number of device types without implementing a separate
+subclass for each.
+
+=head2 Public Driver Methods
 
 These methods are the generic Driver interface, and every driver must
-implement them.  Specific drivers may have additional methods.
+implement them.  Specific drivers may have additional methods related
+to their particular tasks.
 
-=over 2
+=head3 new
 
-=item new
+new() creates, initializes, and returns a new driver.  Specific
+drivers may have different constructor parameters.  The default
+constructor parameters should configure the driver for the most common
+use case.
 
-new() creates and initializes a new driver.  Specific drivers may have
-different constructor parameters.
+=head3 get FILEHANDLE
 
-=item get FILEHANDLE
+get() immediately tries to read information from a FILEHANDLE.  It
+returns an array reference on success---even if nothing was read from
+the FILEHANDLE.  get() returns undef on error, and $! will be set to
+the reason why get() failed.
 
-get() immediately tries to read information from a filehandle.  It
-returns a reference to an array containing whatever it managed to
-read, or an empty array if nothing could be read.  It returns undef on
-error, and $! will be set.
+The returned arrayref will be empty if nothing was read from the
+FILEHANDLE.
 
-The arrayref get() returns is suitable for passing to any
-POE::Filter's get() method.  This is exactly what the ReadWrite wheel
-does with it.
+In an EOF condition, get() returns undef with the numeric value of $!
+set to zero.
+
+The arrayref returned by get() is suitable for passing to any
+POE::Filter's get() or get_one_start() method.  Wheels do exactly this
+internally.
 
 =item put ARRAYREF
 
-put() places raw data chunks into the driver's output queue.  it
-accepts a reference to a list of raw data chunks, and it returns the
-number of octets remaining in its output queue.
+put() accepts an ARRAYREF of raw octet chunks.  These octets are added
+to the dirver's internal output queue or buffer.  put() returns the
+number of octets pending output after the new octets are buffered.
 
 Some drivers may flush data immediately from their put() methods.
 
 =item flush FILEHANDLE
 
-flush() attempts to flush some data from the driver's output queue to
-the FILEHANDLE.  It returns the number of octets remaining in the
-output queue after the flush attempt.
+flush() attempts to write a driver's buffered data to a given
+FIELHANDLE.  The driver should flush as much data as possible in a
+single flush() call.
 
-flush() does the physical write, counterpoint to get's read.  If
-flush() fails for any reason, $! will be set with the reason for its
-failure.  Otherwise $! will be zero.
+flush() returns the number of octets remaining in the driver's output
+queue or buffer after the maximum amount of data has been written.
+
+flush() denotes success or failure by the value of $! after it
+returns.  $! will always numerically equal zero on success.  On
+failure, $! will contain the usual Errno value.  In either case,
+flush() will return the number of octets in the driver's output queue.
 
 =item get_out_messages_buffered
 
-This data accessor returns the number of messages in the driver's
-output queue.  Partial messages are counted as whole ones.
+get_out_messages_buffered() returns the number of messages enqueued in
+the driver's output queue, rounded up to the nearest whole message.
+Some applications require the message count rather than the octet
+count.
+
+Messages are raw octet chunks enqueued by put().  The following put()
+call enqueues two messages for a total of six octets:
+
+  $filter->put( [ "one", "two" ] );
+
+It is possible for a flush() call to write part of a message.  A
+partial message still counts as one message.
 
 =back
 
@@ -92,9 +138,17 @@ output queue.  Partial messages are counted as whole ones.
 The SEE ALSO section in L<POE> contains a table of contents covering
 the entire POE distribution.
 
+L<POE::Wheel> - A base class for POE::Session mix-ins.
+
+L<POE::Filter> - A base class for data parsers and serializers.
+
+L<POE::Driver::SysRW> - A driver that encapsulates sysread() and
+buffered syswrite().
+
 =head1 BUGS
 
 There is no POE::Driver::SendRecv, but nobody has needed one so far.
+sysread() and syswrite() manage to do almost everything people need.
 
 In theory, drivers should be pretty much interchangeable.  In
 practice, there seems to be an impermeable barrier between the
@@ -107,4 +161,3 @@ Please see L<POE> for more information about authors and contributors.
 =cut
 
 # rocco // vim: ts=2 sw=2 expandtab
-# TODO - Redocument.
