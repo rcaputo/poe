df209368632f8bee8e09f4b017d50b62d1b6e0f8
Rocco Caputo <rcaputo@cpan.org>
2010-07-27 06:53:32 -0400

    New version to spackle over failed one.

--- lib/POE/Pipe.pm
+++ lib/POE/Pipe.pm
@@ -7,7 +7,7 @@ package POE::Pipe;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '1.289'; # NOTE - Should be #.### (three decimal places)
+$VERSION = '1.291'; # NOTE - Should be #.### (three decimal places)
 
 use Symbol qw(gensym);
 use IO::Socket qw(
