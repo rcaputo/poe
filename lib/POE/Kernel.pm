rcaputo/poe

commit 41f3b9eca45a2314a6f43723e5e9f71731ef7b3e

Convert the changelog and repository metadata to Git.

lib/POE/Kernel.pm
@@ -2007,8 +2007,8 @@ sub alarm_remove {
   # value when someone needs something useful from it.
 
   return unless defined wantarray;
-  return ( $event->[EV_NAME], $time, @{$event->[EV_ARGS]} ) if wantarray;
-  return [ $event->[EV_NAME], $time, @{$event->[EV_ARGS]} ];
+  return ( $event->[EV_NAME], $time, $event->[EV_ARGS] ) if wantarray;
+  return [ $event->[EV_NAME], $time, $event->[EV_ARGS] ];
 }
 
 # Move an alarm to a new time.  This virtually removes the alarm and

mylib/Makefile-5005.pm
@@ -112,7 +112,7 @@ WriteMakefile(
     COMPRESS => 'gzip -9f',
     SUFFIX   => 'gz',
     PREOP    => (
-      './mylib/svn-log.perl | ' .
+      'git-log.pl | ' .
       '/usr/bin/tee ./$(DISTNAME)-$(VERSION)/CHANGES > ./CHANGES; ' .
       "$^X mylib/gen-meta.perl; " .
       '/bin/cp -f ./META.yml ./$(DISTNAME)-$(VERSION)/META.yml'

mylib/PoeBuildInfo.pm
@@ -25,9 +25,7 @@ use vars qw(@EXPORT_OK);
 
 
 sub CONFIG_REQUIREMENTS () {
-  (
-    "POE::Test::Loops"  => '1.036',
-  )
+  ("POE::Test::Loops"  => '1.036')
 }
 
 sub CORE_REQUIREMENTS () {
@@ -67,7 +65,7 @@ sub DIST_AUTHOR () {
 }
 
 sub DIST_ABSTRACT () {
-  ( 'A portable networking and multitasking framework.' )
+  ( 'Portable, event-loop agnostic eventy networking and multitasking.' )
 }
 
 sub CLEAN_FILES () {
@@ -98,11 +96,11 @@ sub TEST_FILES () {
 }
 
 sub REPOSITORY () {
-  ( 'https://poe.svn.sourceforge.net/svnroot/poe/trunk' )
+  'https://github.com/rcaputo/poe'
 }
 
 sub HOMEPAGE () {
-  ( 'http://poe.perl.org/' )
+  'http://poe.perl.org/'
 }
 
 1;

commit b2661899a157a2f05112c926a7f09265cf1ba193

fix bad commit 41f3b9eca45a2314a6f43723e5e9f71731ef7b3e, caused k_alarms.t to FAIL

lib/POE/Kernel.pm
@@ -2007,8 +2007,8 @@ sub alarm_remove {
   # value when someone needs something useful from it.
 
   return unless defined wantarray;
-  return ( $event->[EV_NAME], $time, $event->[EV_ARGS] ) if wantarray;
-  return [ $event->[EV_NAME], $time, $event->[EV_ARGS] ];
+  return ( $event->[EV_NAME], $time, @{$event->[EV_ARGS]} ) if wantarray;
+  return [ $event->[EV_NAME], $time, @{$event->[EV_ARGS]} ];
 }
 
 # Move an alarm to a new time.  This virtually removes the alarm and