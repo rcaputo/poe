b81728e7d1b76be619c34c0f2940dcbbd44970b8
Rocco Caputo <rcaputo@cpan.org>
2013-09-05 14:36:12 -0400

    Fail better if asked to serialize a non-reference.
    A component was trying to serialize undef() rather than a reference to
    something undefined.  It was causing failure attributed to this
    filter, rather than the user.  Now there's a stack trace to help
    spread the blame.

--- lib/POE/Filter/Reference.pm
+++ lib/POE/Filter/Reference.pm
@@ -10,7 +10,7 @@ use vars qw($VERSION @ISA);
 $VERSION = '1.356'; # NOTE - Should be #.### (three decimal places)
 @ISA = qw(POE::Filter);
 
-use Carp qw(carp croak);
+use Carp qw(carp croak confess);
 
 sub BUFFER    () { 0 }
 sub FREEZE    () { 1 }
@@ -215,6 +215,7 @@ sub put {
   BEGIN { eval { require bytes } and bytes->import; }
 
   my @raw = map {
+    confess "Choking on a non-reference ($_)" unless ref();
     my $frozen = $self->[FREEZE]->($_);
     $frozen = compress($frozen) if $self->[COMPRESS];
     length($frozen) . "\0" . $frozen;
